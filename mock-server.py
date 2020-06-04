809c44c5ce8ba3e49e0f48cf33594ea95fe044ee
Kimmo Kulovesi <kk@arkku.com>
2020-06-04 22:40:35 +0300

    Response modification (merge, replace, delete)

--- mock-server.py
+++ mock-server.py
@@ -1,6 +1,17 @@
+#
+# mock-server.py: A mitmproxy script for mocking server responses.
+# The mock configuration is loaded from a JSON file, e.g.:
+#
+#   mitmdump -s mock-server.py --set mock=test.json -m reverse:https://foo.com/
+#
+# Documentation is a work in progress, see test.json for examples or ask
+# the author (Kimmo Kulovesi) for now.
+#
+
 import json
 import random
 import re
+from typing import Optional
 from mitmproxy import http
 from mitmproxy import ctx
 from mitmproxy import net
@@ -8,7 +19,7 @@ from mitmproxy import net
 def host_matches(host: str, allow) -> bool:
     """
     Return whether `host` matches `allow`.
-    
+
     `allow` may be a string pattern, or a list of such patterns, in which
     case returns True if `host` matches any pattern in `allow`.
 
@@ -66,7 +77,7 @@ def request_matches_config(request: http.HTTPRequest, config: dict) -> bool:
     required_scheme = config.get("scheme", mock_config.get("scheme"))
     if required_scheme and not matches_value_or_list(request.scheme, required_scheme):
         return False
-    required_query = config.get("query", mock_config.get("query"))
+    required_query = config.get("query")
     if required_query:
         query = request.query
         for key in required_query:
@@ -75,14 +86,130 @@ def request_matches_config(request: http.HTTPRequest, config: dict) -> bool:
                 return False
     return True
 
+def is_subset(subset, superset) -> bool:
+    """
+    Return whether `subset` is indeed a subset of `superset`. That is, all
+    items contained in `subset` must be found exactly in `superset`. Any strings
+    in subset may be prefixed with a tilde (`~`) in which case the suffix is
+    interpreted as a regular expression.
+    """
+    if isinstance(subset, dict):
+        return all(key in superset and is_subset(subset[key], superset[key]) for key in subset)
+    elif isinstance(subset, list):
+        return all(any(is_subset(subitem, superitem) for superitem in superset) for subitem in subset)
+    elif isinstance(subset, str):
+        if subset.startswith("~"):
+            allow_re = re.compile(subset[1:], re.M | re.X | re.S)
+            return bool(allow_re.search(str(superset)))
+        else:
+            return str(superset) == subset
+    else:
+        return subset == superset
+
+def content_matches(content: str, allow) -> bool:
+    """
+    Returns whether `content` matches the `allow` criteria.
+
+    `allow` may be of the following types:
+    - a string prefixed with a tilde (`~`), in which case the suffix is
+      interpreted as a regular expression and matched against `content`
+    - any other string, which needs to be a substring of `content`
+    - a dictionary, in which case `content` is interpreted as a JSON object
+      which must be a superset `allow` (see `is_subset`)
+    - a list of any of the above, which must all match
+    """
+    if isinstance(allow, str):
+        if allow.startswith("~"):
+            allow_re = re.compile(allow[1:], re.X)
+            return bool(allow_re.search(content))
+        else:
+            return allow in content
+    elif isinstance(allow, dict):
+        try:
+            content_object = json.loads(content)
+            return is_subset(allow, content_object)
+        except:
+            return False
+    else:
+        for allowed in allow:
+            if not content_matches(content, allowed):
+                return False
+    return True
+
 def response_matches_config(response: http.HTTPResponse, config: dict) -> bool:
     """
     Returns whether `response` is matched by `config`. This checks the following:
 
     - `status` (the HTTP status code)
-    - `error` (boolean true/false
-    - `content`
+    - `content` (a string or a list of strings where _all_ must match)
+
+    For content matching, each string can either be a regular expression denoted
+    by a tilde prefix (`~`), otherwise a substring that must be found exactly.
     """
+    required_status = config.get("status")
+    if required_status and not matches_value_or_list(response.status_code, required_status):
+        return False
+    required_content = config.get("content")
+    if required_content and not content_matches(response.text, required_content):
+        return False
+    return True
+
+def merge_content(merge, content):
+    """
+    Merges `merge` into `content` recursively for dictionaries and lists.
+    """
+    if isinstance(merge, dict):
+        for key in merge:
+            value = merge[key]
+            if isinstance(value, dict):
+                content[key] = merge_content(value, content.get(key, {}))
+            elif isinstance(value, list):
+                content[key] = content.get(key, []) + value
+            else:
+                content[key] = value
+    elif isinstance(merge, list):
+        if isinstance(content, list):
+            content.append(merge)
+        else:
+            content = [ content ] + merge
+    return content
+
+def delete_content(delete, content):
+    """
+    Returns `content` after recursively deleting `delete` from it.
+
+    Any matching dictionary keys are deleted if their value is empty or matches
+    the corresponding value in `content`. For lists, if `delete` has a non-empty
+    list, its elements are compared to the corresponding list in `content`
+    according to `is_subset`, and any matches are deleted from `content`.
+    """
+    if isinstance(delete, dict):
+        for key in delete:
+            value = delete[key]
+            if isinstance(value, dict):
+                if value:
+                    content_value = content.get(key)
+                    if isinstance(content_value, dict):
+                        content[key] = delete_content(value, content_value)
+                else:
+                    content.pop(key, None)
+            elif isinstance(value, list):
+                if value:
+                    content_value = content.get(key)
+                    if isinstance(content_value, list):
+                        content[key] = delete_content(value, content_value)
+                else:
+                    content.pop(key, None)
+            else:
+                if (not value) or content.get(key) == value:
+                    content.pop(key, None)
+    elif isinstance(delete, list):
+        if delete and isinstance(content, list):
+            for deletion in delete:
+                content = [ value for value in content if not is_subset(deletion, value) ]
+        else:
+            content = []
+    return content
 
 def count_based_config(path, config: dict) -> dict:
     """
@@ -109,7 +236,7 @@ def count_based_config(path, config: dict) -> dict:
         result.update(count_config.get("*", {}))
         result.update(count_config.get("even" if (count % 2) == 0 else "odd", {}))
         result.update(count_config.get(str(count), {}))
-        ctx.log.info("Count {} for {}: {}".format(count, path, result))
+        ctx.log.debug("Count {} for {}: {}".format(count, path, result))
     return result
 
 def encode_content(content) -> (bytes, str):
@@ -128,7 +255,6 @@ def encode_content(content) -> (bytes, str):
     if isinstance(content, str):
         try:
             with open(content, "rb") as content_file:
-                ctx.log.info("Reading file: {}".format(content))
                 if content.endswith("html"):
                     content_type = "text/html"
                 elif content.endswith(".xml"):
@@ -157,7 +283,7 @@ def make_response(response, status = 200, content = {}, headers = {}) -> http.HT
      - `type`: The Content-Type header type with or without the charset
      - `charset`: The charset part of the Content-Type header
      - `headers`: A dictionary of HTTP headers
-    
+
     See also: `encode_content`
     """
     if isinstance(response, str):
@@ -194,39 +320,53 @@ def configure(updated) -> None:
         if not mock_config:
             ctx.log.error("No configuration: use --set config.json")
 
-# Called for every incoming request, before passing anything to the remote
-# server. Handling requests allows mocking data for endpoints not present
-# on remote, or modifying the outgoing request.
-def request(flow: http.HTTPFlow) -> None:
-    ctx.log.debug("Request {}: {}".format(flow.request.path, flow.request))
+def resolve_config(flow: http.HTTPFlow, event: str) -> Optional[dict]:
+    """
+    Returns configuration for the event (`request` or `response`) from
+    the flow state and the global mock configuration, or `None` if no
+    configuration item matches the event.
+    """
     path = flow.request.path.split("?")[0]
-    handlers = mock_config.get("request", {})
+    handlers = mock_config.get(event, {})
     path_handler = handlers.get(flow.request.path, handlers.get(path))
     if path_handler is None:
-        return
+        return None
+    is_request = (event == "request")
     config = handlers.get("*", {})
     if isinstance(path_handler, list):
         matched = None
         for handler in path_handler:
             handler_config = {**config, **handler}
-            if request_matches_config(flow.request, handler_config):
+            if request_matches_config(flow.request, handler_config) and (is_request or response_matches_config(flow.response, handler_config)):
                 matched = handler_config
                 break
         if matched is None:
-            return
+            return None
         config = matched
     else:
         config = {**config, **path_handler}
-        if not request_matches_config(flow.request, config):
-            return
+        if not (request_matches_config(flow.request, config) and (is_request or response_matches_config(flow.response, config))):
+            return None
     config.update(count_based_config(path, config))
     random_configs = config.get("random")
     if random_configs:
         config.update(random.choice(random_configs))
-    ctx.log.info("Match {}: {}".format(flow.request.path, config))
+    if config.get("pass", False):
+        return None
+    return config
+
+# Called for every incoming request, before passing anything to the remote
+# server. Handling requests allows mocking data for endpoints not present
+# on remote, or modifying the outgoing request.
+def request(flow: http.HTTPFlow) -> None:
+    ctx.log.debug("Request {}: {}".format(flow.request.path, flow.request))
+    config = resolve_config(flow, "request")
+    if config is None:
+        return
+    ctx.log.debug("Match request: {}".format(flow.request.path))
     modify = config.get("modify")
     if modify:
-        ctx.log.info("Modify: {} -> {}".format(flow.request, modify))
+        ctx.log.info("Modify request: {} -> {}".format(flow.request, modify))
         flow.request.scheme = modify.get("scheme", flow.request.scheme)
         flow.request.host = modify.get("host", flow.request.host)
         flow.request.path = modify.get("path", flow.request.path)
@@ -239,23 +379,77 @@ def request(flow: http.HTTPFlow) -> None:
     response = config.get("response")
     if response:
         flow.response = make_response(response)
-        ctx.log.info("Mock {}: {}".format(flow.request, flow.response))
+        ctx.log.info("Mock {}".format(flow.request.path))
 
 # Called before returning a response from the remote server. This can be
 # used to rewrite responses based on their original contents. For example,
 # the same endpoint may return multiple types of responses and we may wish
 # to mock only some of them, or to mock them in different ways.
 def response(flow: http.HTTPFlow) -> None:
-    path = flow.request.path.split("?")[0]
-    handlers = mock_config.get("response", {})
-    config = {**handlers.get("*", {}), **handlers.get(path, {})}
-    ctx.log.info("Response {}: {}".format(path, flow.request))
-    if not request_matches_config(flow.request, config):
-        return # the request did not match
-    if not response_matches_config(flow.response, config):
-        return # the response did not match
-    config.update(count_based_config(":" + path, config))
-    ctx.log.info("Match {}: {}".format(path, config))
+    ctx.log.debug("Response {}: {}".format(flow.request.path, flow.response))
+    config = resolve_config(flow, "response")
+    if config is None:
+        return
+    ctx.log.debug("Match response: {}".format(flow.request.path))
+    replace = config.get("replace")
+    if replace:
+        response = replace.get("response", replace)
+        if response:
+            flow.response = make_response(response)
+            ctx.log.debug("Replace response {}: {}".format(flow.request.path, flow.response))
+    modify = config.get("modify", [])
+    if isinstance(modify, dict) or isinstance(modify, str):
+        modify = [ modify ]
+    global_modify = mock_config.get("response", {}).get("*", {}).get("modify")
+    if global_modify:
+        if isinstance(global_modify, dict) or isinstance(global_modify, str):
+            global_modify = [ global_modify ]
+        modify = global_modify + modify
+    for modification in modify:
+        if isinstance(modification, dict):
+            content = {}
+            try:
+                content = json.loads(flow.response.text)
+            except ValueError as error:
+                ctx.log.info("Invalid JSON: {}: {}".format(error, flow.response.text))
+            delete = modification.get("delete")
+            replace = modification.get("replace")
+            merge = modification.get("merge")
+            if delete:
+                content = delete_content(delete, content)
+            if replace:
+                if isinstance(replace, str):
+                    try:
+                        with open(replace) as replace_file:
+                            replace = json.load(merge_file)
+                    except FileNotFoundError:
+                        pass
+                if isinstance(replace, dict):
+                    content.update(replace)
+                elif isinstance(content, list) and isinstance(replace, list):
+                    content = replace
+                else:
+                    if isinstance(replace, str):
+                        replace = replace[1:].split(replace[0])
+                    sub_re, replacement = re.compile(replace[0], re.X), replace[1]
+                    flow.response.text = sub_re.sub(replacement, flow.response.text)
+                    try:
+                        content = json.loads(flow.response.text)
+                    except ValueError as error:
+                        ctx.log.error("Invalid JSON: {}: after replace: {}".format(error, replace))
+            if merge:
+                if isinstance(merge, str):
+                    with open(merge) as merge_file:
+                        merge = json.load(merge_file)
+                content = merge_content(merge, content)
+            flow.response.content = json.dumps(content or {}).encode("utf-8")
+        else:
+            if isinstance(modification, str):
+                modification = modification[1:].split(modification[0])
+            sub_re, replacement = re.compile(modification[0], re.X), modification[1]
+            flow.response.text = sub_re.sub(replacement, flow.response.text)
+    if modify:
+        ctx.log.info("Modify response {}: {}".format(flow.request.path, modify))
 
 mock_config = {}
 hit_count = {}
